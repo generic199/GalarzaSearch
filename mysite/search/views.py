e1b1d0e67ddac167bd5de34a6509c190aea1d4e2
Generic <robertfletcher.gmail.com>
2024-10-05 15:36:01 -0400

    views

--- mysite/search/views.py
+++ mysite/search/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def search(request):
+    return HttpResponse("Hello, World. Wait for bobby to implement searching")
+    
